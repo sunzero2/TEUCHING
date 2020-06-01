2aaeb8e1f55ad25c9d2d7d463abec5b9bb362514
kyu <59732399+euf1o@users.noreply.github.com>
2020-06-01 18:04:37 +0900

    1804

--- TeuChingTeuChing/src/main/java/com/borajoin/teuching/member/model/service/MemberService.java
+++ TeuChingTeuChing/src/main/java/com/borajoin/teuching/member/model/service/MemberService.java
@@ -1,5 +1,20 @@
 package com.borajoin.teuching.member.model.service;
 
+import java.util.List;
+import java.util.Map;
+
 public interface MemberService {
 
+	
+	
+	
+	
+	
+	public int insertFile(List<Map<String,Object>>fileData);
+	
+	
+	
+	
+	
+	
 }
--- TeuChingTeuChing/src/main/java/com/borajoin/teuching/member/model/service/MemberServiceImpl.java
+++ TeuChingTeuChing/src/main/java/com/borajoin/teuching/member/model/service/MemberServiceImpl.java
@@ -1,7 +1,50 @@
 package com.borajoin.teuching.member.model.service;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.borajoin.teuching.member.model.dao.MemberDao;
+
+@Service
 public class MemberServiceImpl implements MemberService{
 
+	@Autowired
+	private MemberDao md;
+	
+	@Override
+	public int insertFile(List<Map<String, Object>> fileData) {
+
+		int res = 0;
+		
+		for(Map<String,Object> file : fileData) {
+			
+			md.insertFile(file);
+			
+			MultipartFile mf = (MultipartFile) file.get("file");
+			File f = new File((String)file.get("savePath"));
+			
+			
+			try {
+				
+				mf.transferTo(f);
+				
+			} catch (IllegalStateException | IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
+		}
+		
+		
+		return res;
+	}
 	
 	
 	
