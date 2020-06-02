a0e8bce860a696fd89f49a061ca2df6bb89efefd
violetitgo <63706085+violetitgo@users.noreply.github.com>
2020-06-03 00:41:27 +0900

    이보라

--- TeuChingTeuChing/src/main/java/com/borajoin/teuching/review/model/service/ReviewServiceImpl.java
+++ TeuChingTeuChing/src/main/java/com/borajoin/teuching/review/model/service/ReviewServiceImpl.java
@@ -1,17 +1,28 @@
 package com.borajoin.teuching.review.model.service;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import com.borajoin.teuching.review.model.dao.ReviewDao;
+import com.borajoin.teuching.review.model.vo.Review;
+
+import common.util.Paging;
 
 public class ReviewServiceImpl implements ReviewService {
 
-	
+	ReviewDao rd = new ReviewDao();
 	
 	@Override
 	public Map<String, Object> selectReviewList(String orderby, int currentPage, int cntPerPage) {
 		
 		Map<String, Object> res = new HashMap<>();
 		
+		Paging page = new Paging(rd.contentCnt(), currentPage, cntPerPage);
+		List<Review> nlist = rd.selectReviewList(page, orderby);
+		res.put("paging", page);
+		res.put("nlist", nlist);
+	
 		return res;
 	}
 
