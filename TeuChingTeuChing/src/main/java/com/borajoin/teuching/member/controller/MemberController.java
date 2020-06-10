3176055ebda2121fc664523af2ce1eb4bd804b18
kyu <59732399+euf1o@users.noreply.github.com>
2020-06-11 03:09:28 +0900

    06110309이남규

--- TeuChingTeuChing/src/main/java/com/borajoin/teuching/member/controller/MemberController.java
+++ TeuChingTeuChing/src/main/java/com/borajoin/teuching/member/controller/MemberController.java
@@ -265,51 +265,6 @@ public class MemberController {
 	
 	
 	
-	@RequestMapping("/notice/noticeupload.do")
-	public ModelAndView fileUpload(@RequestParam List<MultipartFile> files, HttpServletRequest request) {
-		
-		ModelAndView mav = new ModelAndView();
-		
-		String root = request.getSession().getServletContext().getRealPath("/");
-			List<Map<String,Object>> fileData = new ArrayList<Map<String,Object>>();
-		
-		HttpSession session = request.getSession();
-
-		int i =0;
-		for(MultipartFile mf :files) {
-			//
-			String savePath = root + "resources\\upload\\";
-			String originFileName = mf.getOriginalFilename();
-			
-			//파일의 이름을 현재시간으로 만들고 확장자 넣기
-			HashMap<String, Object> data = new HashMap<>();
-			SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
-			String renameFileName 
-			= sdf.format(new Date()) + i
-			+ "." + originFileName.substring(originFileName.lastIndexOf(".")+1);
-			
-			//매번 초기화
-			savePath += renameFileName;
-			
-			//map에 다 넣기
-			data.put("originFileName", originFileName);
-			data.put("renameFileName", renameFileName);
-			data.put("savePath", savePath);
-			data.put("file", mf);
-			
-			fileData.add(data);
-			i++;
-		}
-			
-		int res = ms.insertFile(fileData);
-		
-		mav.setViewName("redirect:noticelist.do");
-		
-		return mav;
-	}
-	
-	
-	
 	
 	
 	
