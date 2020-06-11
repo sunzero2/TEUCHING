af7ae15789b3f43998c6c2e9821618c6d4f5e3f0
violetitgo <63706085+violetitgo@users.noreply.github.com>
2020-06-12 02:27:38 +0900

    흑흑

--- TeuChingTeuChing/src/main/java/com/borajoin/teuching/review/model/dao/ReviewDao.java
+++ TeuChingTeuChing/src/main/java/com/borajoin/teuching/review/model/dao/ReviewDao.java
@@ -38,4 +38,56 @@ public class ReviewDao {
 	public int insertReview(Review review) {
 		return sqlSession.insert("Review.insertReview",review);
 	}
+	
+	
+	// 추천 
+	public void recUpdate(String nickname, String no) {
+		
+		System.out.println("실행됨?");
+		Map<String,Object> map = new HashMap<String,Object>();
+		map.put("nickname",nickname);
+		map.put("no",no);
+		sqlSession.update("Review.recUpdate",map);
+		
+	}
+	
+	
+	// reviewrec테이블에 데이터 추가
+	public int insertreviewrrec(String nickname, String no) {
+		System.out.println("실행됨?22");
+		Map<String,Object> map = new HashMap<String,Object>();
+		map.put("nickname",nickname);
+		map.put("no",no);
+		return sqlSession.insert("Review.insertreviewrrec",map);
+		
+	}
+	
+	
+	
+	// 추천수 가져오기
+	public int getLike(String nickname) {
+		System.out.println(nickname);
+		return sqlSession.selectOne("Review.getLike",nickname);
+	}
+	
+	
+	
+	// 추천여부
+	public int reviewrecyn(String nickname, String no) {
+		Map<String,Object> map = new HashMap<String,Object>();
+		map.put("nickname",nickname);
+		map.put("no",no);
+		return (Integer) sqlSession.selectOne("Review.reviewrecyn",map);
+	}
+	
+	
+	// 추천 제거
+	public void recDelete(String nickname, String no) {
+		Map<String,Object> map = new HashMap<String,Object>();
+		map.put("nickname",nickname);
+		map.put("no",no);
+		sqlSession.update("Review.recDelete",map);
+	}
+	
+	
 }
--- TeuChingTeuChing/src/main/java/com/borajoin/teuching/review/model/service/ReviewService.java
+++ TeuChingTeuChing/src/main/java/com/borajoin/teuching/review/model/service/ReviewService.java
@@ -12,9 +12,10 @@ public interface ReviewService {
 	
 	public Map<String,Object> selectReviewList(String orderby, int currentPage, int cntPerPage);
 	public int insertReview(Review review);
-	public int recUpdate();
-	
-	
+	public void recUpdate(String nickname, String no);
+	public int reviewrecyn(String nickname, String no);
+	public void recDelete(String nickname, String no);
+	public int recCount(String nickname, String no);	
 	
 	
 	
